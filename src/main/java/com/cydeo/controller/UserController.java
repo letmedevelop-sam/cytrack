da201bdcf7aaf976e7b9979bfdb321d64a61dd99
SamKar <letmedevelopsam@gmail.com>
2021-06-15 23:20:42 +0300

    admin and instructor fixed

--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -2,6 +2,7 @@ package com.cydeo.controller;
 
 import com.cydeo.service.UserService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -16,12 +17,15 @@ public class UserController {
     }
 
     @GetMapping("/adminList")
-    public String adminList(){
+    public String adminList(Model model){
+        model.addAttribute("admins", userService.listAllUserByRole("Admin"));
         return "user/admin-list";
     }
 
     @GetMapping("/instructorList")
-    public String instructorList(){
+    public String instructorList(Model model){
+
+        model.addAttribute("instructors", userService.listAllUserByRole("Instructor"));
         return "user/instructor-list";
     }
 
