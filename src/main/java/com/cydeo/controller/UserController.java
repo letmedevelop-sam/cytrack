f6868fe94c62f9ae298205cc1ba41dc50510631f
SamKar <letmedevelopsam@gmail.com>
2021-06-15 20:41:57 +0300

    User set

---
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -0,0 +1,22 @@
+package com.cydeo.controller;
+
+import com.cydeo.service.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+    UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/adminList")
+    public String adminList(){
+        return "user/admin-list";
+    }
+}
