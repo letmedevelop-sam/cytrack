f51b80412c8b42d29651270f233cfb8a9a93038f
SamKar <letmedevelopsam@gmail.com>
2021-06-19 00:26:48 +0300

    Lesson Controller added

---
+++ src/main/java/com/cydeo/controller/LessonController.java
@@ -0,0 +1,25 @@
+package com.cydeo.controller;
+
+import com.cydeo.service.LessonService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/lesson")
+public class LessonController {
+
+    LessonService lessonService;
+
+    public LessonController(LessonService lessonService) {
+        this.lessonService = lessonService;
+    }
+
+//    @GetMapping("/lessonList")
+//    public String lessonList(Model model){
+//
+//        model.addAttribute("lessons", lessonService.getLessonsAndInstructorsMap());
+//
+//        return "lesson/lesson-list";
+//    }
+
+}
